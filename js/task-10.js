1d215f606744ce3be74696d5d6de8b96d69642c5
hallekh <lekhnovska.halyna@gmail.com>
2023-12-20 18:56:13 +0100

    task-5(10)

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.getElementById("controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  if (!amount) {
+    alert("Please enter a valid number");
+    return;
+  }
+  destroyBoxes();
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
