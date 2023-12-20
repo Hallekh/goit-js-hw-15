53f5e8d6ee4cf8ad3e7e20cc284fd0a7550d7542
hallekh <lekhnovska.halyna@gmail.com>
2023-12-20 17:45:36 +0100

    task-3(8)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (!email.value || !password.value) {
+    alert("усі поля мають бути заповнені");
+    return;
+  }
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+});
